f998979cb4747f4e63c333922572a31a647491fe
Francesco <micheli@cern.ch>
2016-03-18 15:41:40 +0100

    first commit

---
+++ EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/runEEShashlik.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/runEEShashlik.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/CreateTree.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/CreateTree.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashActionInitialization.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashActionInitialization.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashCalorHit.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashCalorHit.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashCalorimeterSD.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashCalorimeterSD.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashDetectorConstruction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashDetectorConstruction.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashEventAction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashEventAction.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashPrimaryGeneratorAction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashPrimaryGeneratorAction.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/EEShashRunAction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/EEShashRunAction.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/G4EmUserPhysics.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/G4EmUserPhysics.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/MyMaterials.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/MyMaterials.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/SteppingAction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/SteppingAction.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/TrackInformation.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/TrackInformation.cc.o"
+  "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/src/TrackingAction.cc" "/shome/micheli/geant4/EEShashlikSimulation/H4OpticalSmall_singleFibre/cmake/CMakeFiles/runEEShashlik.dir/src/TrackingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4_STORE_TRAJECTORY"
+  "G4VERBOSE"
+  "G4UI_USE"
+  "G4VIS_USE"
+  "G4UI_USE_TCSH"
+  "G4INTY_USE_XT"
+  "G4VIS_USE_RAYTRACERX"
+  "G4INTY_USE_QT"
+  "G4UI_USE_QT"
+  "G4VIS_USE_OPENGLQT"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGL"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
