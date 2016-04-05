151e679a8416107aadfecf3d1408d6963b42a09c
Francesco <micheli@cern.ch>
2016-04-05 09:40:30 +0200

    fixing path

--- Analyzer/myroot.sh
+++ Analyzer/myroot.sh
@@ -7,7 +7,7 @@ source /afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/setup.sh
 source /afs/cern.ch/sw/lcg/external/geant4/10.0.p01/x86_64-slc6-gcc47-opt/CMake-setup.sh
 export CXX=/afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/bin/g++
 export CC=/afs/cern.ch/sw/lcg/contrib/gcc/4.7/x86_64-slc6/bin/gcc
-source /swshare/ROOT/root_v5.34.18_slc6_amd64_py26_pythia6/bin/thisroot.sh 
+source /mnt/t3nfs01/data01/swshare/ROOT/root_v5.34.18_slc6_amd64_py26_pythia6/bin/thisroot.sh 
 #source /swshare/ROOT/root_v5.34.32_precompiled/root/bin/thisroot.sh 
 
 
